164ea361d3c740df8e5ec8909de3add55c5a6829
Joonsu Yoon <bananamilk452@gmail.com>
2025-09-19 15:53:06 +0900

    원 카드 드로우 컴포넌트 추가

--- src/components/tarot/OneCardDraw.tsx
+++ src/components/tarot/OneCardDraw.tsx
@@ -1,9 +1,48 @@
+import { useMutation } from "@tanstack/react-query";
+import { useEffect, useState } from "react";
+
+import { TAROT_SPREADS } from "~/constant";
 import { useCards } from "~/context/CardContext";
+import { createTarotResponse } from "~/server/tarot";
 
 import { CardWithTitle } from "../Card";
+import { TarotLoading } from "../TarotLoading";
+import { Button } from "../ui/button";
 
 export function OneCardDraw() {
-  const { selection } = useCards();
+  const [showLoading, setShowLoading] = useState(false);
+
+  const { question, selection, reset } = useCards();
+  const delay = 1000;
+  const spread = TAROT_SPREADS[0];
+
+  const {
+    data,
+    mutate: getTarotReading,
+    status,
+  } = useMutation({
+    mutationFn: async (data: {
+      question: string;
+      cards: string[];
+      spread: string;
+    }) => {
+      return await createTarotResponse({ data: data });
+    },
+  });
+
+  useEffect(() => {
+    getTarotReading({
+      question,
+      cards: selection.map(
+        (card) => `${card.name} (${card.flip ? "역방향" : "정방향"})`,
+      ),
+      spread,
+    });
+
+    setTimeout(() => {
+      setShowLoading(true);
+    }, delay);
+  }, []);
 
   return (
     <div className="flex flex-col flex-wrap items-center gap-6">
@@ -12,8 +51,55 @@ export function OneCardDraw() {
         side="back"
         flip={selection[0].flip}
         withAnimation
-        delay={1000}
+        delay={delay}
       />
+
+      {showLoading && (
+        <>
+          <TarotLoading
+            className={
+              status === "pending"
+                ? "opacity-100 transition-opacity duration-500"
+                : "hidden opacity-0 transition-opacity"
+            }
+          />
+          {status === "success" && (
+            <>
+              <div className="p-4 md:w-4/5 lg:w-1/2">
+                <h2 className="mb-1 text-lg font-semibold">카드의 의미</h2>
+                <p className="whitespace-pre-wrap break-keep">
+                  {data.cards_meaning}
+                </p>
+
+                <hr className="my-6 border-gray-600" />
+
+                <h2 className="mb-1 text-lg font-semibold">
+                  질문과 카드의 관련성
+                </h2>
+                <p className="whitespace-pre-wrap break-keep">
+                  {data.relevance}
+                </p>
+
+                <hr className="my-6 border-gray-600" />
+
+                <h2 className="mb-1 text-lg font-semibold">결론</h2>
+                <p className="whitespace-pre-wrap break-keep">
+                  {data.conclusion}
+                </p>
+              </div>
+              <Button onClick={reset}>다시 하기</Button>
+            </>
+          )}
+          {status === "error" && (
+            <>
+              <div>오류가 발생했어요. 다시 시도해주세요.</div>
+              <Button className="mt-4" onClick={reset}>
+                다시 하기
+              </Button>
+            </>
+          )}
+        </>
+      )}
     </div>
   );
 }
